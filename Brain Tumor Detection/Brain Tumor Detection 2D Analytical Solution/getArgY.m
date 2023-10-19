1d1b43916ec65d5a6c5d72ff6ab4a197a906b413
Anil Aksu <aaa293@cornell.edu>
2023-10-19 12:10:20 +0300

    Delete Brain Tumor Detection directory

--- Brain Tumor Detection/Brain Tumor Detection 2D Analytical Solution/getArgY.m
+++
@@ -1,9 +0,0 @@
-function [ k_y ] = getArgY( n,H )
-%this function returns nth argument in y direction
-% n is the order of the argument in y direction 
-% y is the coordinate
-% H  is the domain length in y direction 
-k_y=(pi/H +2*(n-1)*pi/H);
-
-end
-
